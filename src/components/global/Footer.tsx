164372c66603f2628922cc4ca19329c230944322
qienzdork <qienzdork502@gmail.com>
2024-08-09 13:49:50 +0700

    Update Footer.tsx

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
     <footer className="flex items-center justify-center lg:h-56 h-36">
       <div className="flex items-center">
         <div className="rounded-full flex items-center w-full p-2 border border-border gap-2">
-          <Link href={"https://x.com/mfraiyan"} target="_blank" className={buttonClass.icon}>
+          <Link href={"https://x.com/qienz502"} target="_blank" className={buttonClass.icon}>
             <FaXTwitter size={32} />
           </Link>
-          <Link href={"https://t.me/pengaruhbesar"} target="_blank" className={buttonClass.icon}>
+          <Link href={"https://t.me/airdropjatake"} target="_blank" className={buttonClass.icon}>
             <FaTelegramPlane size={32} />
           </Link>
-          <Link href={"https://github.com/muhammadfathurraiyan"} target="_blank" className={buttonClass.icon}>
+          <Link href={"https://github.com/qienzdork"} target="_blank" className={buttonClass.icon}>
             <FaGithub size={32} />
           </Link>
         </div>
